bd79a427414a20b1c24c7256ae09af60fee64b66
Joseph Ameer Aziz <joseph.ameer21@gmail.com>
2022-04-24 02:22:27 +0200

    Made some changes

--- process.c
+++ process.c
@@ -3,18 +3,40 @@
 /* Modify this file as needed*/
 int remainingtime;
 
+
+
 int main(int agrc, char * argv[])
 {
     initClk();
-    
+    signal(SIGUSR1,handler1);
+    signal(SIGUSR2,handler2);
+
     //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
+    
+  
+    remainingtime = argv[1];
     while (remainingtime > 0)
     {
-        // remainingtime = ??;
+        remainingtime--;
     }
     
+   return 0;
+}
+
+    void handler1(int signum)
+{
+ //Stop
+ 
+  signal(SIGUSR1,handler1);
+}
+
+void handler2(int signum)
+{
+ //Continue
+ 
+  signal(SIGUSR2,handler2);
+}
+
     destroyClk(false);
     
-    return 0;
-}
+ 
